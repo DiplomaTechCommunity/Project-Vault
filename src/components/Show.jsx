7af212af7ead9b8b6a08d434641a73573537cf94
Manjiri Milind Chavande <manjiri.c@fyle.in>
2023-08-14 13:25:58 +0530

    added UI for dearch bar and made it responsive

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -2,20 +2,19 @@ import Projects from './Projects'
 import ProjectData from './ProjectData';
 import "./Projects.css";
 import { useState } from 'react';
+import { FaSearch } from "react-icons/fa";
+import "./Show.css";
 
 const Show = () => {
   const [searchQuery, setSearchQuery] = useState('');
-  const keys = ['project_name','description','tech_stack'];
+  const keys = ['project_name', 'description', 'tech_stack'];
 
   const filteredProjects = ProjectData.filter((item) =>
-  keys.some((key) =>
+    keys.some((key) =>
       Array.isArray(item[key]) &&
       item[key].some((data) => data.toLowerCase().includes(searchQuery.toLowerCase()))
-  )
-);
-
-
-
+    )
+  );
 
   return (
     <div className='tut-container'>
@@ -27,18 +26,22 @@ const Show = () => {
         <p>Welcome to the DTC Project Vault, a dynamic platform dedicated to sharing your projects and sparking innovation. Here, we believe that knowledge shared is innovation multiplied. Join our community in inspiring and learning from diverse projects, as we collaboratively shape a future driven by creativity and technological prowess</p>
       </div>
 
+      {/* Search Bar */}
       <div className='search-bar'>
-        <input
-          type='text'
-          id='searchInput'
-          placeholder='search...'
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
+        <div className='input-wrapper'>
+          <FaSearch id="search-icon" />
+          <input
+            type='text'
+            id='searchInput'
+            placeholder='search...'
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </div>
       </div>
 
       <div className='project-container'>
-      {filteredProjects.map((project) => (
+        {filteredProjects.map((project) => (
           <Projects
             key={project.id}
             imgsrc={project.imgsrc}
